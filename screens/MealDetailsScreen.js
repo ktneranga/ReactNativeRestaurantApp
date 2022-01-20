aa9c51ddec73080ea97bf3e791e9455ece9a42e1
it18256888 <it18256888@my.sliit.lk>
2022-01-20 22:48:50 +0530

    Adding screens

---
+++ screens/MealDetailsScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+const MealDetailsScreen = (props) => {
+    return(
+        <View>
+            <Text>Meal details screen</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    screen : {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
+
+export default MealDetailsScreen;
