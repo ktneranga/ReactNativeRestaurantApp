cd14f7659466297851ed4692cf083b02749d32d3
it18256888 <it18256888@my.sliit.lk>
2022-01-31 23:28:46 +0530

    load meals

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,27 +1,30 @@
 import React from 'react';
-import {Text, View, StyleSheet, Button, Platform} from 'react-native';
-import { CATEGORIES } from '../data/dummy-data';
+import {Text, View, StyleSheet, Button, Platform, FlatList} from 'react-native';
+import { CATEGORIES,MEALS} from '../data/dummy-data';
 import Color from '../constants/Color';
 
 const CategoryMealsScreen = (props) => {
     const catId = props.navigation.getParam('categoryId');
 
-    const selectedCategory = CATEGORIES.find((cat)=>cat.id === catId);
+    // const selectedCategory = CATEGORIES.find((cat)=>cat.id === catId);
+
+    const displayedMeals = MEALS.filter(meal => meal.categoryIds.indexOf(catId)>=0); //if true this meal has catId
+
+    const renderItem = (itemData) => { //itemData object passed by the flatlist
+        return(
+            <View>
+                <Text>{itemData.item.title}</Text>
+            </View>
+        );
+    }
 
     return(
         <View style={styles.screen}>
-            <Text>Category Meals Screen</Text>
-            <Text>{selectedCategory.title}</Text>
-            <Button title='Go to Meal Details' onPress={()=>{
-                props.navigation.navigate({routeName: 'MealDetails'});
-                //push needs to navigate to the same screen, when we need 
-                //to load different content to the same screen
-                // props.navigation.push('MealDetails');
-            }} />
-            <Button title='Go Back' onPress={()=>{
-                // props.navigation.goBack();
-                props.navigation.pop();
-            }} />
+            <FlatList
+                data={displayedMeals}
+                renderItem={renderItem}
+                keyExtractor={(item,index) => item.id}
+            />
         </View>
     );
 }
