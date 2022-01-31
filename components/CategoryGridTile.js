5215fa71a8114abee66cf250dbf73816ee93be22
it18256888 <it18256888@my.sliit.lk>
2022-01-31 21:50:26 +0530

    .

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity, Platform, TouchableNativeFeedback, Touchable} from 'react-native';
 
 const CategoryGridTile = (props) => {
+
+    let TouchableCmp = TouchableOpacity;
+
+    if(Platform.OS == 'android' && Platform.Version >= 21){
+        TouchableCmp = TouchableNativeFeedback;
+    }
+
     return (
-        <TouchableOpacity 
-            style={styles.gridItem} 
-            onPress={props.onPress}
-        >
-            <View style={ {...styles.container, ...{backgroundColor: props.color}} }>
-                <Text style={styles.title}>{props.title}</Text>
-            </View>
-        </TouchableOpacity>
+        <View style={styles.gridItem}>
+            <TouchableCmp 
+                style={styles.gridItem} 
+                onPress={props.onPress}
+            >
+                <View style={ {...styles.container, ...{backgroundColor: props.color}} }>
+                    <Text style={styles.title} numberOfLines={2}>{props.title}</Text>
+                </View>
+            </TouchableCmp>
+        </View>
     );
 }
 
@@ -18,17 +27,25 @@ const styles = StyleSheet.create({
     gridItem: {
         flex: 1,
         margin: 15,
-        height: 150
+        height: 150,
+        borderRadius: 10
     },
     container: {
         flex: 1,
         borderRadius: 10,
         justifyContent: 'flex-end',
         alignItems: 'flex-end',
-        padding: 15
+        padding: 15,
+        shadowColor: 'black',
+        shadowOpacity: 0.26,
+        shadowOffset: {width: 0, height: 2},
+        shadowRadius: 10,
+        elevation: 3
     },
     title: {
-        fontFamily: 'open-sans-bold'
+        fontFamily: 'open-sans-bold',
+        fontSize: 20,
+        textAlign: 'right'
     }
 });
 
