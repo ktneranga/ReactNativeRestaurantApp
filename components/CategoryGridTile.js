70e7ed16b1ee2287194144dda8211c1f6bd004c5
it18256888 <it18256888@my.sliit.lk>
2022-02-08 14:33:50 +0530

    changes

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -45,7 +45,7 @@ const styles = StyleSheet.create({
     },
     title: {
         fontFamily: 'open-sans-bold',
-        fontSize: 20,
+        fontSize: 18,
         textAlign: 'right'
     }
 });
