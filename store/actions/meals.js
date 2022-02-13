2d525273541dd9224d99ced4f82200e590d6b919
it18256888 <it18256888@my.sliit.lk>
2022-02-13 09:54:59 +0530

    .

--- store/actions/meals.js
+++ store/actions/meals.js
@@ -2,10 +2,15 @@
 
 //SETTING UP unique identifier
 export const TOGGLE_FAVORITE = 'TOGGLE_FAVORITE'; //to switch a feature , add or remove
+export const SET_FILTERS = 'SET_FILTERS';
 
 //action creator
 export const toggleFavorite = (id) => {
     return {type: TOGGLE_FAVORITE, mealId: id}; //action => JS object that describe the action
 };
+
+export const setFilters = (filterSettings) => { 
+    return {type: SET_FILTERS, filters: filterSettings} //filterSettings objects
+}
 
 //action describe about what happened => here user clicked on favorite toggle button
